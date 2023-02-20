f69a4f310317a382776500173d956326cd18729a
Okeke Chioma <okekegeraldenne@gmail.com>
2023-02-20 13:32:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//question 3
+let classCat = "art";
+
+let scienceSubjects = "Physics, Chemistry, Biology, Technical Drawing"
+let socialScienceSubjects = "Accounting, Commerce, Marketing, Geography"
+let artsSubjects = "Government, Economics, Literature, History"
+let generalSubjects = "English, Mathematics"
+
+if (classCat == "science"){
+    console.log(scienceSubjects + ", " + generalSubjects)
+} 
+else if (classCat == "art") {
+    console.log(artsSubjects + ", " + generalSubjects)
+}
+else if (classCat == "socialScience") {
+    console.log(socialScienceSubjects + ", " + generalSubjects)
+}
+else {
+    console.log(generalSubjects)
+}
+
+//question 5
---
+++ readme.md
@@ -0,0 +1,18 @@
+1. 
+    a. Arithmetic Operators - +, -, *, /, %, **, ++, --
+    b. Assignment Operators - =, +=, -=, *=, /=
+    c. Comparison Operators - >, <, >=, <=, ==, ===, !==
+    d. Logical Operators - &, |, !
+    e. Bitwise Operators - ^(XOR), ~(NOT), &(AND), |(OR), <<(Zero fill left shift), >>(Signed right shift), >>>(Zero fill right shift)
+
+2. 
+    a. Arithmetic Operators - x - y, x * y
+    b. Assignment Operators - x = 10, x +=  5
+    c. Comparison Operators - x >= 1, x <= 1
+    d. Logical Operators - x >= 1 || x <= 1, 10 === "10" && 10 > 4
+    e. Bitwise Operators - 5 << 1, 5 ^ 1
+
+4. result = 1 8 15
+
+
+
